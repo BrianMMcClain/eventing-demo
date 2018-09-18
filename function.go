a9c9b591e07d35be5d159d9710ac1dd2e9a42e9e
Brian McClain <brianmmcclain@gmail.com>
2018-09-18 14:20:49 -0400

    Initial commit

---
+++ function.go
@@ -0,0 +1,61 @@
+/*
+Copyright 2018 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func handlePost(rw http.ResponseWriter, req *http.Request) {
+
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(string(body))
+
+	// 	// decode the pubsub message
+	// 	//	var event map[string]string
+	// 	var event Event
+	// 	if err := json.Unmarshal(body, &event); err != nil {
+	// 		log.Printf("Failed to unmarshal event: %s", err)
+	// 		return
+	// 	}
+
+	// 	// decode pubsub payload
+	// 	rawEvent, _ := base64.StdEncoding.DecodeString(event.Data)
+
+	// 	// decode iot data
+	// 	data, _ := base64.StdEncoding.DecodeString(string(rawEvent))
+
+	// 	// decode the pubsub message payload
+	// 	var payload EventPayload
+	// 	if err := json.Unmarshal(data, &payload); err != nil {
+	// 		log.Printf("Failed to unmarshal payload: %s", err)
+	// 		return
+	// 	}
+
+	// 	log.Printf("Data sent by device %q: [metric: %d, on: %v]",
+	// 		event.Attributes["deviceId"], payload.Metric, payload.SentOn)
+}
+
+func main() {
+	http.HandleFunc("/", handlePost)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
